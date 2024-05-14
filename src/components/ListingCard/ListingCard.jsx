46dd45c7e6615abce79efe1e623fbb5685daa599
Bpost129 <Bpost129@yahoo.com>
2024-05-14 17:46:49 -0400

    change toys and games to toys & games in the new listing form to match backend data, implement scrolling title

--- src/components/ListingCard/ListingCard.jsx
+++ src/components/ListingCard/ListingCard.jsx
@@ -10,7 +10,7 @@ const ListingCard = ({ listing }) => {
       <div className={styles.listingContainer} key={listing._id}>
         <div className={styles.titleAndCategory}>
           <h2 className={styles.listingCategory}>{listing.category}</h2>
-          <h2 className={styles.listingTitle}>{listing.title}</h2>
+          <h2 className={styles.listingTitle}><span>{listing.title}</span></h2>
         </div>
         <div className={styles.listingImage} style={{backgroundImage: `url(${listing.photos[0]})`}} alt="Listing Picture" ></div>
         <h2 className={styles.listingCreatedAt}>{`Created On: ${(new Date(listing.createdAt).toDateString())}`}</h2>
