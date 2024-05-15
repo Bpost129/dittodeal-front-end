3dbfd2f85d20d40f57c5d8e0f3c1f96d3df205ad
bS-ui <brandonxsears@gmail.com>
2024-05-15 15:10:04 -0400

    Styles listing card and profile page

--- src/components/ListingCard/ListingCard.jsx
+++ src/components/ListingCard/ListingCard.jsx
@@ -7,7 +7,22 @@ const ListingCard = ({ listing }) => {
   
   return (
     <main className={styles.mainContainer}> 
-      <div className={styles.listingContainer} key={listing._id}>
+      <div 
+        className={styles.listingContainer} 
+        key={listing._id} 
+        style={listing.category === "Vehicles" ? {backgroundImage: "url(" + "src/assets/images/vehicles.png" + ")"} 
+        : listing.category === "Apparel" ? {backgroundImage: "url(" + "src/assets/images/apparel.png" + ")"}
+        : listing.category === "Electronics" ? {backgroundImage: "url(" + "src/assets/images/electronics.png" + ")"}
+        : listing.category === "Entertainment" ? {backgroundImage: "url(" + "src/assets/images/entertainment.png" + ")"}
+        : listing.category === "Garden & Outdoor" ? {backgroundImage: "url(" + "src/assets/images/garden_and_outdoor.png" + ")"}
+        : listing.category === "Home Goods" ? {backgroundImage: "url(" + "src/assets/images/home_goods.png" + ")"}
+        : listing.category === "Home Improvement" ? {backgroundImage: "url(" + "src/assets/images/home_improvement.png" + ")"}
+        : listing.category === "Music" ? {backgroundImage: "url(" + "src/assets/images/music.png" + ")"}
+        : listing.category === "Office Supplies" ? {backgroundImage: "url(" + "src/assets/images/office_supplies.png" + ")"}
+        : listing.category === "Pet Supplies" ? {backgroundImage: "url(" + "src/assets/images/pet_supplies.png" + ")"}
+        : listing.category === "Sporting Goods" ? {backgroundImage: "url(" + "src/assets/images/sporting_goods.png" + ")"}
+        : {backgroundImage: "url(" + "src/assets/images/toys_and_games.png" + ")"}
+      }>
         <div className={styles.titleAndCategory}>
           <h2 className={styles.listingCategory}>{listing.category}</h2>
           <h2 className={styles.listingTitle}><span>{listing.title}</span></h2>
